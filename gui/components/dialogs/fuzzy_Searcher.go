2876456d080e4f22374bef7e46afd7d96c907c11
Elias Gill <eliasgill42@gmail.com>
2024-01-04 16:13:46 -0300

    refactor: Refactoring awfull code of the fuzzy finder

--- gui/components/dialogs/fuzzy_Searcher.go
+++ gui/components/dialogs/fuzzy_Searcher.go
@@ -14,51 +14,49 @@ import (
 	"github.com/elias-gill/walldo-in-go/wallpaper"
 )
 
-var (
-	data          = []string{}
-	resultsWidget *widget.List
-)
-
-// refresh the content search list with every keystroke.
-func entryChanged(entry string) {
-	data = []string{}
-	imagesList := utils.GetImagesList()
-
-	if len(entry) >= 1 {
-		// search for the matching results
-		matches := matching.FindAll(entry, imagesList)
-		// display the results
-		for i := 0; i < len(matches); i++ {
-			data = append(data, imagesList[matches[i].Idx])
-		}
-	}
-
-	resultsWidget.Refresh()
-}
-
 // Create a new Fuzzy finder dialog and display it.
 func NewFuzzyDialog() {
-	// search input
-	searcherWiget := widget.NewEntry()
-	searcherWiget.SetPlaceHolder("Search Image")
-	searcherWiget.OnChanged = entryChanged
+	var (
+		data = []string{}
+	)
 
 	// list of results
-	resultsWidget = widget.NewList(
+	resultsWidget := widget.NewList(
 		func() int {
 			return len(data)
 		},
 		func() fyne.CanvasObject {
 			return widget.NewLabel("template")
 		},
-		func(i widget.ListItemID, o fyne.CanvasObject) {
-			o.(*widget.Label).SetText(data[i])
+		func(id widget.ListItemID, canvas fyne.CanvasObject) {
+			canvas.(*widget.Label).SetText(data[id])
 		})
+
 	resultsWidget.OnSelected = func(id int) {
 		wallpaper.SetFromFile(strings.Clone(data[id]))
 	}
 
-	cont := container.New(layout.NewBorderLayout(searcherWiget, nil, nil, nil), searcherWiget, resultsWidget)
+	// search input
+	searchInput := widget.NewEntry()
+	searchInput.SetPlaceHolder("Search Image")
+	searchInput.OnChanged =
+		func(entry string) {
+			imagesList := utils.GetImagesList()
+			data = []string{}
+
+			if len(entry) >= 1 {
+				// search for the matching results
+				matches := matching.FindAll(entry, imagesList)
+				// display the results
+				for i := 0; i < len(matches); i++ {
+					data = append(data, imagesList[matches[i].Idx])
+				}
+			}
+
+			resultsWidget.Refresh()
+		}
+
+	cont := container.New(layout.NewBorderLayout(searchInput, nil, nil, nil), searchInput, resultsWidget)
 	dial := dialog.NewCustom("Fuzzy search", "Cancel", cont, globals.Window)
 	dial.Resize(fyne.NewSize(500, 300))
 	dial.Show()
