bc9bd7259d3ab865f1732af468234944b81cb87c
Elias Gill <eliasgill42@gmail.com>
2025-03-22 15:02:58 -0300

    feat: uninstall command line flag

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"image/color"
+	"os"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -13,6 +16,32 @@ import (
 )
 
 func main() {
+	uninstallFlag := flag.Bool("uninstall", false, "Uninstall the executable by deleting itself")
+	flag.Parse()
+
+	if *uninstallFlag {
+		// Get the path to the current executable
+		exePath, err := os.Executable()
+		if err != nil {
+			fmt.Println("Error getting executable path:", err)
+			return
+		}
+
+		// Attempt to delete the executable
+		err = os.Remove(exePath)
+		if err != nil {
+			fmt.Println("Error deleting executable:", err)
+			return
+		}
+
+		fmt.Println("Executable uninstalled successfully:", exePath)
+		return
+	}
+
+	startGui()
+}
+
+func startGui() {
 	app := app.NewWithID("Walldo")
 	window := app.NewWindow("Walldo")
 
