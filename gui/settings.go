d9fdd47f7d258808cd29eaf728004dc2bec98937
Elias Gill <eliasgill42@gmail.com>
2025-03-24 09:48:47 -0300

    feat: new layout for putting inputs along with buttons

--- gui/settings.go
+++ gui/settings.go
@@ -118,8 +118,10 @@ func newConfigWindow(callback func()) {
 			sizeSelectorContainer,
 			widget.NewSeparator(),
 			pathsSelectorLabel,
-			pathInput,
-			fileExplorerButton,
+			container.New(
+				&PriorityLayout{},
+				pathInput,
+				fileExplorerButton),
 		),
 		nil, nil, nil,
 		pathsList,
@@ -147,4 +149,74 @@ func newConfigWindow(callback func()) {
 	)
 
 	configDialog.Show()
+}
+
+// Custom Layout to arange an input and buttons on the side
+type PriorityLayout struct {
+	// PrimaryRatio defines the fraction of space the first element occupies (0.0 to 1.0)
+	PrimaryRatio float32
+}
+
+func (l *PriorityLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
+	if len(objects) == 0 {
+		return
+	}
+
+	if l.PrimaryRatio <= 0 {
+		l.PrimaryRatio = 0.85 // Default to half the space if ratio not set
+	}
+
+	primarySize := float32(0)
+	secondarySize := float32(0)
+	remaining := len(objects) - 1
+
+	primarySize = size.Width * l.PrimaryRatio
+	secondarySize = (size.Width - primarySize) / float32(remaining)
+
+	// Position first element
+	objects[0].Resize(fyne.Size{Width: primarySize, Height: size.Height})
+	objects[0].Move(fyne.Position{X: 0, Y: 0})
+
+	// Position remaining elements
+	xPos := primarySize
+	for i := 1; i < len(objects); i++ {
+		objects[i].Resize(fyne.Size{Width: secondarySize, Height: size.Height})
+		objects[i].Move(fyne.Position{X: xPos, Y: 0})
+		xPos += secondarySize
+	}
+}
+
+func (l *PriorityLayout) MinSize(objects []fyne.CanvasObject) fyne.Size {
+	if len(objects) == 0 {
+		return fyne.Size{Width: 0, Height: 0}
+	}
+
+	minSize := fyne.Size{Width: 0, Height: 0}
+
+	// Get the minimum size of the primary object
+	primaryMin := objects[0].MinSize()
+
+	// Get the maximum minimum size of secondary objects
+	secondaryMin := fyne.Size{Width: 0, Height: 0}
+	for i := 1; i < len(objects); i++ {
+		objMin := objects[i].MinSize()
+		if objMin.Width > secondaryMin.Width {
+			secondaryMin.Width = objMin.Width
+		}
+		if objMin.Height > secondaryMin.Height {
+			secondaryMin.Height = objMin.Height
+		}
+	}
+
+	minSize.Width = primaryMin.Width + secondaryMin.Width*float32(len(objects)-1)
+	minSize.Height = max(primaryMin.Height, secondaryMin.Height)
+
+	return minSize
+}
+
+func max(a, b float32) float32 {
+	if a > b {
+		return a
+	}
+	return b
 }
